c1c9b48e6dc017a0c4810a69e8350ecbbb7c1c5e
Arthur Barrett <abarrett@fas.harvard.edu>
2013-04-11 18:54:55 +0000

    Fix js whitespace (tabs -> spaces).

--- harmony/apps/lab/static/js/src/piano/keyboard.js
+++ harmony/apps/lab/static/js/src/piano/keyboard.js
@@ -1,13 +1,13 @@
 define(['jquery', 'lodash', 'raphael'], function($, _, Raphael) {
 
-    // Keyboard constructor function.
-    // 
-    // Usage: 
-    //
-    //  var keyboard = new Keyboard(88);
-    //  keyboard.render();
-    //  $('#piano').append(keyboard.el);
-    //
+	// Keyboard constructor function.
+	// 
+	// Usage: 
+	//
+	//	var keyboard = new Keyboard(88);
+	//	keyboard.render();
+	//	$('#piano').append(keyboard.el);
+	//
 	var Keyboard = function(numKeys) {
 		if(typeof numKeys !== 'undefined') {
 			if(!this.keyboardSizes.hasOwnProperty(this.numKeys)) {
@@ -16,29 +16,29 @@ define(['jquery', 'lodash', 'raphael'], function($, _, Raphael) {
 			this.numKeys = numKeys;
 		}
 
-        this.el = $('<div class="keyboard"></div>');
+		this.el = $('<div class="keyboard"></div>');
 
-        this.paper = Raphael(this.el.get(0), this.width, this.height);
+		this.paper = Raphael(this.el.get(0), this.width, this.height);
 	};
 
 	_.extend(Keyboard.prototype, {
-        // Dimensions of the keyboard.
-        width: 800,
-        height: 150,
+		// Dimensions of the keyboard.
+		width: 800,
+		height: 150,
 
-        // White keys may represent one of 7 possible notes in the musical alphabet. 
-        whiteKeys: 'ABCDEFG',
+		// White keys may represent one of 7 possible notes in the musical alphabet. 
+		whiteKeys: 'ABCDEFG',
 
-        // Black keys represent half steps between the main notes (sharp or flat).
-        blackKey: '-',
+		// Black keys represent half steps between the main notes (sharp or flat).
+		blackKey: '-',
 
-        // Black keys are arranged in groups of 2's and 3's and appear after some white keys.
-        blackKeysAfter: 'ACDFG',
+		// Black keys are arranged in groups of 2's and 3's and appear after some white keys.
+		blackKeysAfter: 'ACDFG',
 
-        // Number of keys on the keyboard
+		// Number of keys on the keyboard
 		numKeys: 49,
 
-        // Maps valid keyboard sizes to configuration parameters. 
+		// Maps valid keyboard sizes to configuration parameters. 
 		keyboardSizes: {
 			25: { 'firstNote': 'C' }, 
 			37: { 'firstNote': 'C' }, 
@@ -48,16 +48,16 @@ define(['jquery', 'lodash', 'raphael'], function($, _, Raphael) {
 			88: { 'firstNote': 'A' }
 		},
 
-        // Returns the next white key note in the musical alphabet. 
-        nextWhiteKey: function(whiteKey) {
-            var nextIndex = this.whiteKeys.indexOf(whiteKey) + 1;
-            if(nextIndex === -1) {
-                throw new Error("Invalid whiteKey. No such white key.");
-            }
-            return this.whiteKeys.charAt(nextIndex % this.whiteKeys.length);
-        },
+		// Returns the next white key note in the musical alphabet. 
+		nextWhiteKey: function(whiteKey) {
+			var nextIndex = this.whiteKeys.indexOf(whiteKey) + 1;
+			if(nextIndex === -1) {
+				throw new Error("Invalid whiteKey. No such white key.");
+			}
+			return this.whiteKeys.charAt(nextIndex % this.whiteKeys.length);
+		},
 
-        // Returns a sequence of white and black keys for a given size.
+		// Returns a sequence of white and black keys for a given size.
 		generateKeys: function(whiteKey, size) {
 			var nextWhiteKey = this.nextWhiteKey(whiteKey);
 			if(size == 0) {
@@ -68,54 +68,54 @@ define(['jquery', 'lodash', 'raphael'], function($, _, Raphael) {
 			return [whiteKey].concat(this.generateKeys(nextWhiteKey, size - 1));
 		},
 
-        // Returns the first note for the current keyboard size.
-        getFirstNote: function() {
-            return this.keyboardSizes[this.numKeys]['firstNote'];
-        },
+		// Returns the first note for the current keyboard size.
+		getFirstNote: function() {
+			return this.keyboardSizes[this.numKeys]['firstNote'];
+		},
 
-        // Returns a key sequence for the keyboard.
+		// Returns a key sequence for the keyboard.
 		getKeys: function() {
 			return this.generateKeys(this.getFirstNote(), this.numKeys);
 		},
 
-        // Filters a list of keys by white/black.
-        filterKeys: function(keys, type) {
-            var blackKey = this.blackKey;
-            var filterBy;
-            if(type === 'white') {
-                filterBy = function(key) { return key !== blackKey; };
-            } else if(type === 'black') {
-                filterBy = function(key) { return key === blackkey; };
-            } else {
-                throw new Error("Invalid key type");
-            }
+		// Filters a list of keys by white/black.
+		filterKeys: function(keys, type) {
+			var blackKey = this.blackKey;
+			var filterBy;
+			if(type === 'white') {
+				filterBy = function(key) { return key !== blackKey; };
+			} else if(type === 'black') {
+				filterBy = function(key) { return key === blackkey; };
+			} else {
+				throw new Error("Invalid key type");
+			}
 
-            return _.filter(keys, filterBy);
-        },
-
-        // Returns the total number of white keys.
-        getNumWhiteKeys: function(keys) {
-            return this.filterKeys(keys, 'white').length;
-        },
-
-        // Returns the total number of black keys.
-        getNumBlackKeys: function(keys) {
-            return this.filterKeys(keys, 'black').length;
-        },
-
-        // Renders the keyboard.
-		render: function() { 
-            this._render();
-            return this;
+			return _.filter(keys, filterBy);
 		},
 
-        // Helper function for rendering.
+		// Returns the total number of white keys.
+		getNumWhiteKeys: function(keys) {
+			return this.filterKeys(keys, 'white').length;
+		},
+
+		// Returns the total number of black keys.
+		getNumBlackKeys: function(keys) {
+			return this.filterKeys(keys, 'black').length;
+		},
+
+		// Renders the keyboard.
+		render: function() { 
+			this._render();
+			return this;
+		},
+
+		// Helper function for rendering.
 		_render: function() {
-            var paper = this.paper;
+			var paper = this.paper;
 			var width = this.width;
 			var height = this.height;
 			var keys = this.getKeys();
-            var numWhiteKeys = this.getNumWhiteKeys(keys);
+			var numWhiteKeys = this.getNumWhiteKeys(keys);
 			var keyboardEl = paper.rect(0, 0, width, height);
 
 			var eachKeys = function(keys, callback) {
