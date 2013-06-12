e7e379e8e2b6f019abf4d43a137d68591a0e062a
Arthur Barrett <abarrett@fas.harvard.edu>
2013-06-12 12:09:51 -0400

    commenting out url patterns that arent used anymore

--- harmony/urls.py
+++ harmony/urls.py
@@ -9,9 +9,8 @@ urlpatterns = patterns('',
     url(r'^$', 'harmony.apps.lab.views.index', name='index'),
 	#url(r'^login/$', 'django_openid_auth.views.login_begin', name='openid-login'),
 	#url(r'^login-complete/$', 'django_openid_auth.views.login_complete', name='openid-complete'),
-	url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/',}, name='logout'),
-	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', 
-		{'document_root': '/Applications/MAMP/htdocs/lab/lab/static'}),
+	#url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/',}, name='logout'),
+	#url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/Applications/MAMP/htdocs/lab/lab/static'}),
     # url(r'^lab/', include('lab.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
