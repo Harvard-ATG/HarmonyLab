773dab50d6320a446f3633dd013d0b2c497354d4
Arthur Barrett <abarrett@fas.harvard.edu>
2013-07-01 19:26:45 +0000

    fix tab-nav dependency

--- harmony/static/js/src/tab-nav.js
+++ harmony/static/js/src/tab-nav.js
@@ -1,41 +1,41 @@
-$(document).ready(function() {
-  $('#tab-nav-1 > ul > li > a').click(function() {
-    var $parentItem = $(this).parent(),
-        slideAmt = $(this).next().width(),
-        direction;
+define(['jquery'], function($) {
+	$(document).ready(function() {
+	$('#tab-nav-1 > ul > li > a').click(function() {
+		var $parentItem = $(this).parent(),
+			slideAmt = $(this).next().width(),
+			direction;
 
-    if (parseInt($parentItem.css('marginLeft'), 10) < 0) {
-      direction = '+=';
-      $(this).removeClass('expanded');
-    } else {
-      $(this).addClass('expanded');
-      direction = '-=';
-    }
-    $parentItem
-      .animate({marginLeft: direction + slideAmt}, 00);
-    return false;
-  });
- 
+		if (parseInt($parentItem.css('marginLeft'), 10) < 0) {
+		  direction = '+=';
+		  $(this).removeClass('expanded');
+		} else {
+		  $(this).addClass('expanded');
+		  direction = '-=';
+		}
+		$parentItem
+		  .animate({marginLeft: direction + slideAmt}, 00);
+		return false;
+	  });
+	 
+	});
+
+	$(document).ready(function() {
+		$('#tab-nav-2 > ul > li > a').click(function() {
+		var $parentItem = $(this).parent(),
+			slideAmt = $(this).next().width(),
+			direction;
+
+		if (parseInt($parentItem.css('marginLeft'), 10) > 0) {
+		  direction = '-=';
+		  $(this).removeClass('expanded');
+		} else {
+		  $(this).addClass('expanded');
+		  direction = '+=';
+		}
+		$parentItem
+		  .animate({marginLeft: direction + slideAmt}, 00);
+		return false;
+	  });
+	 
+	});
 });
-
-
-$(document).ready(function() {
-  $('#tab-nav-2 > ul > li > a').click(function() {
-    var $parentItem = $(this).parent(),
-        slideAmt = $(this).next().width(),
-        direction;
-
-    if (parseInt($parentItem.css('marginLeft'), 10) > 0) {
-      direction = '-=';
-      $(this).removeClass('expanded');
-    } else {
-      $(this).addClass('expanded');
-      direction = '+=';
-    }
-    $parentItem
-      .animate({marginLeft: direction + slideAmt}, 00);
-    return false;
-  });
- 
-});
-
