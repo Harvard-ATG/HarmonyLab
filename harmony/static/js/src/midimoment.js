fddd78163e3abf81e4d8d6a4a894d3420a2ee213
Arthur Barrett <abarrett@fas.harvard.edu>
2013-07-09 21:02:17 +0000

    add stub module

---
+++ harmony/static/js/src/midimoment.js
@@ -0,0 +1,14 @@
+define(function() {
+	// This module's role is to model a "moment in time" of the performance
+	// in terms of the relevant MIDI control changes and note states.
+	//
+	// The module should be chainable by some means so that arbitrary moments
+	// can be captured and later traversed.
+	//
+	// This module should have a simple interface for querying the notes that
+	// are currently playing and the pedals that are in effect.
+	//
+	// This module should be immutable.
+	//
+	// This module will collaborate with the notation view.
+});
