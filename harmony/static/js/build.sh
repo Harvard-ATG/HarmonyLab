77de5a7ddd51a57bcde41afb15c2085bbf1b01b3
Arthur Barrett <abarrett@edx.org>
2013-08-01 14:17:59 -0400

    update build.sh comment

--- harmony/static/js/build.sh
+++ harmony/static/js/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/sh
 
-# npm -g install requirejs
-r.js -o build.js optimize=none
+# Note: must have node.js installed
+# To install the optimizer "r.js": npm -g install requirejs
+
+mkdir -v build
+r.js -o build.js optimize=none # remove optimize=none to minify
