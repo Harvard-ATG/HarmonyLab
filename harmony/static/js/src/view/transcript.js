06f5f0567f0783f9dcf53bd16ddfaec9215b1b23
Arthur Barrett <abarrett@fas.harvard.edu>
2013-09-18 21:19:49 +0000

    refactor transcript

--- harmony/static/js/src/view/transcript.js
+++ harmony/static/js/src/view/transcript.js
@@ -2,17 +2,18 @@
 define([
 	'jquery',
 	'lodash', 
-	'app/view/transcript/plain_notation'
-], function($, _, PlainNotation) {
+	'app/view/transcript/plain_sheet'
+], function($, _, PlainSheet) {
 	"use strict";
 
+	// Responsible for displaying the sheet music
+	// and laying out related components.
 	var Transcript = function(config) {
 		this.init(config);
 	};
 
 	_.extend(Transcript.prototype, {
-		sheets: [],
-		analyzers: [],
+		sheet: null,
 		layout: {
 			width: 520,
 			height: 355
@@ -25,9 +26,7 @@ define([
 			this.el[0].width = this.getWidth(); 
 			this.el[0].height = this.getHeight(); 
 
-			this.initMethods();
-			this.initComponents();
-			this.initListeners();
+			this.initSheet();
 		},
 		initConfig: function() {
 			var required = ['chords', 'keySignature'];
@@ -39,67 +38,24 @@ define([
 				}
 			}, this);
 		},
-		initComponents: function() {
+		initSheet: function() {
 			var config = {
+				transcript: this,
 				chords: this.chords, 
 				keySignature: this.keySignature
 			};
-			if(this.sheets.length === 0) {
-				this.sheets = [this.sheetFactory('plain')];
-			}
-			this.initSheets(this, config);
-			this.initAnalyzers(this, config);
-		},
-		initMethods: function() {
-			this.initSheets = this._invokeOn(this.getSheets, 'init');
-			this.clearSheets = this._invokeOn(this.getSheets, 'clear');
-			this.renderSheets = this._invokeOn(this.getSheets, 'render');
-
-			this.initAnalyzers = this._invokeOn(this.getAnalyzers, 'init');
-			this.clearAnalyzers = this._invokeOn(this.getAnalyzers, 'clear');
-			this.renderAnalyzers = this._invokeOn(this.getAnalyzers, 'render');
-		},
-		initListeners: function() {},
-		clear: function() {
-			this.clearSheets();
-			this.clearAnalyzers();
+			this.sheet = new PlainSheet(config);
 		},
 		render: function() { 
-			this.clear();
-			this.renderSheets();
-			this.renderAnalyzers();
+			this.sheet.clear();
+			this.sheet.render();
 			return this;
-		},
-		sheetFactory: function(name) {
-			var factoryMap = {'plain': PlainNotation};
-			if(factoryMap[name]) {
-				return new factoryMap[name]();
-			}
-			return false;
-		},
-		analyzerFactory: function(name) {
-			return false;
-		},
-		getSheets: function() {
-			return this.sheets;
-		},
-		getAnalyzers: function() {
-			return this.analyzers;
 		},
 		getWidth: function() {
 			return this.layout.width;
 		},
 		getHeight: function() {
 			return this.layout.height;
-		},
-		_invokeOn: function(delegatesFn, method) {
-			var that = this;
-			return function() {
-				var delegates = delegatesFn.call(that);
-				for(var i = 0, len = delegates.length; i < len; i++) {
-					delegates[i][method].apply(delegates[i], arguments); 
-				}
-			};
 		}
 	});
 
