143fb60da3353da5cf5288d62ec6eea18bcb6bad
Arthur Barrett <abarrett@fas.harvard.edu>
2014-07-13 22:55:39 +0000

    add comments

--- harmony/static/js/src/components/ui/piano.js
+++ harmony/static/js/src/components/ui/piano.js
@@ -42,19 +42,39 @@ define([
 
 	PianoComponent.prototype = new Component();
 
+	/**
+	 * Initializes the component.
+	 *
+	 * @return undefined
+	 */
 	PianoComponent.prototype.initComponent = function() {
 		this.el = $('<div class="keyboard-wrapper"></div>');
 		this.initListeners();
 	};
 
+	/**
+	 * Initializes listeners.
+	 *
+	 * @return undefined
+	 */
 	PianoComponent.prototype.initListeners = function() {
 		this.subscribe(EVENTS.BROADCAST.KEYBOARD_SIZE, this.onKeyboardChange);
 	};
 
+	/**
+	 * Removes listeners.
+	 *
+	 * @return undefined
+	 */
 	PianoComponent.prototype.removeListeners = function() {
 		this.unsubscribe(EVENTS.BROADCAST.KEYBOARD_SIZE, this.onKeyboardChange);
 	};
 
+	/**
+	 * Renders the toolbar, keyboard, and pedals.
+	 *
+	 * @return this
+	 */
 	PianoComponent.prototype.render = function() {
 		this.el.empty();
 		_.invoke(this.components, "render");
@@ -64,12 +84,28 @@ define([
 		return this;
 	};
 
+	/**
+	 * Convenience method to render this object to a 
+	 * location on the page given by the selector.
+	 *
+	 * @param {string|jQuery} selector
+	 * @return this
+	 */
 	PianoComponent.prototype.renderTo = function(selector) {
 		$(selector).append(this.render().el);
 		this.updateWidth();
 		return this;
 	};
 
+	/**
+	 * Handles a keyboard change event that should change the
+	 * size of the keyboard. 
+	 *
+	 * This involves removing the old keyboard and replacing it
+	 * with a new one with the given size.
+	 *
+	 * @return undefined
+	 */
 	PianoComponent.prototype.onKeyboardChange = function(size) {
 		var oldKeyboardComponent = this.getComponent('keyboard');
 		var newKeyboardComponent = new KeyboardComponent({numberOfKeys: size});
@@ -83,6 +119,12 @@ define([
 		this.updateWidth();
 	};
 
+	/**
+	 * Updates the width the piano wrapper element to match
+	 * the size of the keyboard element.
+	 *
+	 * @return undefined
+	 */
 	PianoComponent.prototype.updateWidth = function() {
 		var keyboardComponent = this.getComponent('keyboard');
 		var offset = keyboardComponent.keyboardEl.position();
