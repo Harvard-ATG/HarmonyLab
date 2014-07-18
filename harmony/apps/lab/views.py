50743aff7d4d7a9a9484cdc238c0e53f37542648
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2014-07-18 10:59:05 -0400

    Add django_auth_lti icommons library for using application as an LTI tool.  Upgrade version of Django from 1.5 to 1.6.x.  Add a tool_config view for XML configuration endpoint of tool - used by Canvas to determine display of tool link.  Set up "lab" application with it's own urls.py file and include those from top-level urls.py file.  Disable CSRF middleware to allow LTI tool to POST to index view.

--- harmony/apps/lab/views.py
+++ harmony/apps/lab/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.generic import View, TemplateView
-
+from django.core.urlresolvers import reverse
+from ims_lti_py.tool_config import ToolConfig
 from .exercise import Exercise
 
 import json
@@ -12,12 +13,18 @@ REQUIRE_JS_CONTEXT = {
     'REQUIREJS_CONFIG': json.dumps(settings.REQUIREJS_CONFIG)
 }
 
+
 class HomeView(TemplateView):
     template_name = "piano.html"
+
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context.update(REQUIRE_JS_CONTEXT)
         return context
+
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
 
 class ExerciseView(View):
     def get(self, request, exercise_id, *args, **kwargs):
@@ -31,3 +38,36 @@ class ExerciseView(View):
 
     def get_exercise(self, exercise_id):
         return Exercise().load(exercise_id)
+
+
+class ToolConfigView(View):
+    """
+    Need something here
+    """
+    def get(self, request, *args, **kwargs):
+        if request.is_secure():
+            host = 'https://' + request.get_host()
+        else:
+            host = 'http://' + request.get_host()
+
+        url = host + reverse('lab:index')
+
+        lti_tool_config = ToolConfig(
+            title='Harmony Labs',
+            launch_url=url,
+            secure_launch_url=url,
+        )
+        # this is how to tell Canvas that this tool provides a course navigation link:
+        account_nav_params = {
+            'enabled': 'true',
+            # optionally, supply a different URL for the link:
+            # 'url': 'http://library.harvard.edu',
+            'text': 'Harmony Labs',
+        }
+        lti_tool_config.set_ext_param('canvas.instructure.com', 'privacy_level', 'public')
+        lti_tool_config.set_ext_param('canvas.instructure.com', 'course_navigation', account_nav_params)
+        lti_tool_config.description = 'This LTI tool allows instructors to create and link blogs to a course site.'
+
+        resp = HttpResponse(lti_tool_config.to_xml(), content_type='text/xml', status=200)
+        return resp
+
--- harmony/settings/common.py
+++ harmony/settings/common.py
@@ -111,11 +111,17 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'cached_auth.Middleware',
+    'django_auth_lti.middleware.LTIAuthMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'django_auth_lti.backends.LTIAuthBackend',
 )
 
 ROOT_URLCONF = 'harmony.urls'
@@ -143,7 +149,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     #'django_openid_auth',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 	'harmony.apps.lab',
