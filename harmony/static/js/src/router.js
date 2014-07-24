6050794e5d9584606aed885e9104fc46cfd446ca
Arthur Barrett <abarrett@fas.harvard.edu>
2014-07-24 14:12:23 -0400

    add base_url to router

--- harmony/static/js/src/router.js
+++ harmony/static/js/src/router.js
@@ -1,7 +1,25 @@
-define(['jquery', 'app/components/app/play', 'app/components/app/exercise'], function($) {
+define([
+	'module', 
+	'jquery', 
+	'app/components/app/play', 
+	'app/components/app/exercise'
+], function(
+	module, 
+	$
+) {
+	var MODULE_CONFIG = module.config();
+	var BASE_URL = '/';
+	if("base_url" in MODULE_CONFIG && MODULE_CONFIG.base_url) {
+		BASE_URL = MODULE_CONFIG.base_url;
+	}
+
+	var match = function(path) {
+		return new RegExp("^" + BASE_URL + path, "i");
+	};
+
 	var ROUTES = [
-		{url: /^\/lab\/?$/, app: 'app/components/app/play'},
-		{url: /^\/lab\/exercise\/\d+$/, app: 'app/components/app/exercise'}
+		{url: match("lab/?$"), app: 'app/components/app/play'},
+		{url: match("lab/exercise/\\d+$"), app: 'app/components/app/exercise'}
 	];
 
 	var router = function() {
