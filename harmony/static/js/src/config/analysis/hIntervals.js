23079c3fb58228d6d03b7c34a8a79de8bcd5ce31
Arthur Barrett <abarrett@fas.harvard.edu>
2014-11-18 15:09:52 -0500

    Updated analysis tables provided by Rowland.

--- harmony/static/js/src/config/analysis/hIntervals.js
+++ harmony/static/js/src/config/analysis/hIntervals.js
@@ -14,11 +14,11 @@ define({
 	9: {"label": "M6", "description": "major 6th", "stepwise": "5", "spellbass": "iG_", "root": "_", "rootstepwise": "_"},
 	10: {"label": "m7", "description": "minor 7th", "stepwise": "6", "spellbass": "iA_", "root": "_", "rootstepwise": "_"},
 	11: {"label": "M7", "description": "major 7th", "stepwise": "6", "spellbass": "iF_", "root": "_", "rootstepwise": "_"},
-	12: {"label": "8", "description": "octave", "stepwise": "7", "spellbass": "iD_", "root": "_", "rootstepwise": "_"},
+	12: {"label": "P8", "description": "octave", "stepwise": "7", "spellbass": "iD_", "root": "_", "rootstepwise": "_"},
 	13: {"label": "m9", "description": "minor 9th", "stepwise": "8", "spellbass": "iE_", "root": "_", "rootstepwise": "_"},
 	14: {"label": "M9", "description": "major 9th", "stepwise": "8", "spellbass": "iG_", "root": "_", "rootstepwise": "_"},
 	15: {"label": "m10", "description": "minor 10th", "stepwise": "9", "spellbass": "iE_", "root": "_", "rootstepwise": "_"},
 	16: {"label": "M10", "description": "major 10th", "stepwise": "9", "spellbass": "iC_", "root": "_", "rootstepwise": "_"},
 	19: {"label": "P12", "description": "perfect 12th", "stepwise": "11", "spellbass": "iC_", "root": "_", "rootstepwise": "_"},
-	24: {"label": "15", "description": "fifteenth", "stepwise": "14", "spellbass": "iD_", "root": "_", "rootstepwise": "_"}
+	24: {"label": "P15", "description": "fifteenth", "stepwise": "14", "spellbass": "iD_", "root": "_", "rootstepwise": "_"}
 });
