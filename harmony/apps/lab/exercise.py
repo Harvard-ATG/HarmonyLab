65d5f98315ce28a7d92348145d34c9000cbd9593
Arthur Barrett <abarrett@fas.harvard.edu>
2015-03-05 11:29:21 -0500

    Fixed exercise loader to work with nested structures.

--- harmony/apps/lab/exercise.py
+++ harmony/apps/lab/exercise.py
@@ -15,19 +15,22 @@ class Exercise:
         base_path = os.path.dirname(os.path.realpath(__file__))
         self.exercise_base_path = os.path.join(base_path, 'exercises', 'json')
 
-    def getExerciseFile(self, exercise_id):
+    def getExerciseFilePath(self, exercise_id):
+        '''Returns the full path to the exercise file.'''
         exercise_path = exercise_id.split('/')
         return os.path.join(self.exercise_base_path, *exercise_path) + ".json"
 
-    def getExercisePath(self, exercise_id):
+    def getExerciseFileDir(self, exercise_id):
+        '''Returns the full path to the exercise file's directory.'''
         exercise_path = exercise_id.split('/')
         return os.path.split(os.path.join(self.exercise_base_path, *exercise_path))[0]
 
     def load(self, exercise_id):
+        '''Loads the exercise file data and determines the exercise in the sequence.'''
         if self.loaded:
             return self
 
-        exercise_file = self.getExerciseFile(exercise_id)
+        exercise_file = self.getExerciseFilePath(exercise_id)
         try:
             with open(exercise_file) as f:
                 data = f.read()
@@ -45,27 +48,36 @@ class Exercise:
         return self
 
     def getNextExercise(self, exercise_id):
-        '''Returns the next exercise after the given exercise ID, or None.'''
+        '''
+        Returns a string that is the ID of the next exercise in the sequence, 
+        or None if it's the last exercise.
 
-        exercise_path = self.getExercisePath(exercise_id)
-        exercise_list = os.listdir(exercise_path)
-        print exercise_path, exercise_list
-        sorted_exercise_list = sorted(exercise_list, key=lambda e: e.lower())
+        Notes: 
+            - The order of exercises is determined by an alpha-numeric sort (case-insensitive).
+            - If an exercise is nested in a directory, the next exercise will be from that directory listing.
+        '''
+
+        exercise_head, exercise_tail = os.path.split(exercise_id) 
+        exercise_list = os.listdir(self.getExerciseFileDir(exercise_id))
+        exercise_list = [
+            os.path.join(exercise_head, e.replace('.json', '')) 
+            for e in sorted(exercise_list, key=lambda e: e.lower())
+        ]
 
         next_index = -1
-        for index, item in enumerate(sorted_exercise_list):
-            item = item.replace('.json', '')
-            if exercise_id == item:
+        for index, exercise in enumerate(exercise_list):
+            if exercise_id == exercise:
                 next_index = index + 1
                 break
 
         next_exercise = None
         if next_index >= 0 and next_index < len(exercise_list):
-            next_exercise = exercise_list[next_index].replace('.json', '')
+            next_exercise = exercise_list[next_index]
 
         return next_exercise
 
     def as_json(self, pretty=False):
+        '''Returns the exercise data as a JSON string.'''
         if pretty:
             return json.dumps(self.data, indent=4, separators=(',', ': '), sort_keys=True)
         return json.dumps(self.data)
