727dfdc84dbe281eba7180bdff8ba68e98b5ce16
Arthur Barrett <abarrett@fas.harvard.edu>
2015-03-24 13:20:28 -0400

    Load first exercise in the directory if possible.

--- harmony/apps/lab/exercise.py
+++ harmony/apps/lab/exercise.py
@@ -10,10 +10,20 @@ class Exercise:
     
     def __init__(self, exercise_id):
         self.file = None
-        self.data = None
+        self.data = {}
         self.loaded = False
         self.exercise_base_path = os.path.join(Exercise.BASE_PATH, 'exercises', 'json')
         self.exercise_id = exercise_id
+        self.exerciseList = self.getExerciseList()
+
+        # If a directory is given, try to set the exercise ID to the first one in the directory
+        if self.exerciseIdEndsWithSlash(exercise_id):
+            if len(self.exerciseList) > 0:
+                self.exercise_id = self.exerciseList[0]['id']
+
+    def exerciseIdEndsWithSlash(self, exercise_id):
+        '''Returns true if the exercise id ends with a forward slash.'''
+        return exercise_id.split('/')[-1] == ''
 
     def load(self):
         '''Loads the exercise file data and determines the exercise in the sequence.'''
@@ -28,25 +38,12 @@ class Exercise:
         except IOError as e:
             raise ExerciseError("Error loading exercise. I/O error({0}): {1}".format(e.errno, e.strerror))
 
-
-        next_exercise = self.getNextExercise()
-        if next_exercise is None:
-            self.data['nextExercise'] = ''
-        else:
-            self.data['nextExercise'] = self.getExerciseUrl(next_exercise['id'])
-            
-        exercise_list = self.getExerciseList()
-        if exercise_list is None:
-            self.data['exerciseList'] = ''
-        else:
-            self.data['exerciseList'] = exercise_list
+        self.data['nextExercise'] = self.getExerciseUrlFor('next')
+        self.data['previousExercise'] = self.getExerciseUrlFor('previous')
+        self.data['exerciseList'] = self.exerciseList
 
         return self
-    
-    def getExerciseUrl(self, exercise_id):
-        '''Returns the URL for the exercise.'''
-        return reverse('lab:exercise', kwargs={"exercise_id":exercise_id})  
- 
+
     def getExerciseFilePath(self):
         '''Returns the full path to the exercise file.'''
         exercise_path = self.exercise_id.split('/')
@@ -63,13 +60,16 @@ class Exercise:
         exercise_list = os.listdir(self.getExerciseFileDir())
         filtered_exercise_list = [e.replace('.json', '') for e in exercise_list if e.endswith(".json")]
         sorted_exercise_list = sorted(filtered_exercise_list, key=lambda e: e.lower())
-        return [{
-            "name":name,
+
+        self.exerciseList = [{
             "id": os.path.join(head, name),
+            "name":name,
             "parent": head,
             "url": self.getExerciseUrl(os.path.join(head, name)),
             "selected": os.path.join(head, name) == self.exercise_id,
         } for name in sorted_exercise_list]
+
+        return self.exerciseList
     
     def getFirstExercise(self):
         '''Returns the first exercise, or None.'''
@@ -85,7 +85,7 @@ class Exercise:
             return None
         return exercise_list[-1]       
 
-    def getNextExercise(self):
+    def getNextExercise(self, direction=1):
         '''
         Returns a string that is the ID of the next exercise in the sequence, 
         or None if it's the last exercise.
@@ -100,7 +100,7 @@ class Exercise:
         next_index = -1
         for index, exercise in enumerate(exercise_list):
             if self.exercise_id == exercise['id']:
-                next_index = index + 1
+                next_index = index + direction 
                 break
 
         next_exercise = None
@@ -108,6 +108,25 @@ class Exercise:
             next_exercise = exercise_list[next_index]
 
         return next_exercise
+
+    def getExerciseUrlFor(self, which):
+        '''Returns the URL of the next exercise.'''
+        exercise = None
+        if which == 'next':
+            exercise = self.getNextExercise(1)
+        if which == 'previous':
+            exercise = self.getNextExercise(-1)
+        elif which == 'first':
+            exercise = self.getFirstExercise()
+        elif which == 'last':
+            exercise = self.getLastExercise()
+        if exercise is None:
+            return ''
+        return self.getExerciseUrl(exercise['id'])
+    
+    def getExerciseUrl(self, exercise_id):
+        '''Returns the URL for the exercise.'''
+        return reverse('lab:exercise', kwargs={"exercise_id":exercise_id})  
 
     def as_json(self, pretty=False):
         '''Returns the exercise data as a JSON string.'''
--- harmony/apps/lab/urls.py
+++ harmony/apps/lab/urls.py
@@ -4,7 +4,7 @@ from .views import PlayView, ExerciseView, LTIToolConfigView, LTILaunchView
 urlpatterns = patterns(
     '',
     url(r'^$', PlayView.as_view(), name='index'),
-    url(r'^exercise/(?P<exercise_id>.+[^\/])$', ExerciseView.as_view(), name="exercise"),
+    url(r'^exercise/(?P<exercise_id>.*)$', ExerciseView.as_view(), name="exercise"),
     url(r'^lti-launch$', LTILaunchView.as_view(), name='lti-launch'),
     url(r'^lti-tool-config$', LTIToolConfigView.as_view(), name='lti-tool-config'),
 )
