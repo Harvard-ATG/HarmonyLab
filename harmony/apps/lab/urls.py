679d6e55f9860a5ac16039fd2bb6b298d7ce62b6
Arthur Barrett <abarrett@fas.harvard.edu>
2015-05-20 17:18:44 -0400

    Cleanup urls.py

--- harmony/apps/lab/urls.py
+++ harmony/apps/lab/urls.py
@@ -7,20 +7,30 @@ from views import LTIToolConfigView, LTILaunchView
 urlpatterns = patterns(
     '',
     url(r'^$', PlayView.as_view(), name='index'),
-    url(r'^api$', APIView.as_view(), name="api"),
-    url(r'^api/v1/exercises$', APIExerciseView.as_view(), name="api-exercises"),
-    url(r'^api/v1/groups$', APIGroupView.as_view(), name="api-groups"),
-    url(r'^exercises/(?P<group_name>[a-zA-Z0-9_\-.]+)/(?P<exercise_name>\d+)$', ExerciseView.as_view(), name="exercises"),
-    url(r'^exercises$', ExerciseView.as_view()),
-    url(r'^exercises/(?P<group_name>[a-zA-Z0-9_\-.]+)$', ExerciseView.as_view(), name="exercise-groups"),
+
+    # Course Exercises
+    url(r'^courses/(?P<course_id>\w+)/manage$', ManageView.as_view(), name="course-manage"),
+    url(r'^courses/(?P<course_id>\w+)/authcheck$', views.check_course_authorization, name="course-authorization-check"),
     url(r'^courses/(?P<course_id>\w+)/exercises/(?P<group_name>[a-zA-Z0-9_\-.]+)/(?P<exercise_name>\d+)$', ExerciseView.as_view(), name="course-exercises"),
     url(r'^courses/(?P<course_id>\w+)/exercises/(?P<group_name>[a-zA-Z0-9_\-.]+)$', ExerciseView.as_view(), name="course-exercise-groups"),
     url(r'^courses/(?P<course_id>\w+)/exercises$', ExerciseView.as_view()),
-    url(r'^courses/(?P<course_id>\w+)/manage$', ManageView.as_view(), name="course-manage"),
-    url(r'^courses/(?P<course_id>\w+)/authcheck$', views.check_course_authorization, name="course-authorization-check"),
+
+    # Non-Course Exercises
     url(r'^manage$', ManageView.as_view(), name="manage"),
+    url(r'^exercises/(?P<group_name>[a-zA-Z0-9_\-.]+)/(?P<exercise_name>\d+)$', ExerciseView.as_view(), name="exercises"),
+    url(r'^exercises/(?P<group_name>[a-zA-Z0-9_\-.]+)$', ExerciseView.as_view(), name="exercise-groups"),
+    url(r'^exercises$', ExerciseView.as_view()),
+
+    # API 
+    url(r'^api$', APIView.as_view(), name="api"),
+    url(r'^api/v1/exercises$', APIExerciseView.as_view(), name="api-exercises"),
+    url(r'^api/v1/groups$', APIGroupView.as_view(), name="api-groups"),
+
+    # LTI 
     url(r'^lti-launch$', LTILaunchView.as_view(), name='lti-launch'),
     url(r'^lti-config$', LTIToolConfigView.as_view(), name='lti-config'),
+
+    # Logout
     url(r'^logout$', views.logout_view, name="logout"),
     url(r'^logged-out$', views.logged_out_view, name="logged-out"),
 )
